b4dd351ee752d8e843f01c6b9bdd5430804f57bc
Rhys Frazier <rhysrfrazier@gmail.com>
2023-10-14 13:25:06 -0400

    All CRUD routes up and tested

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser')
 const db = require('./db')
 
 const authorController = require ('./controllers/authorController')
+const novelController = require('./controllers/novelController')
+const specialController = require('./controllers/specialController')
 
 const PORT = process.env.PORT || 3001
 
@@ -15,10 +17,23 @@ app.use(express.json())
 app.use(bodyParser.json())
 
 app.get('/', (req,res) => res.send('Welcome?'))
+
 app.get('/authors', authorController.getAllAuthors)
 app.get('/authors/:id', authorController.getOneAuthor)
 app.post('/authors', authorController.createAuthor)
 app.put('/authors/:id', authorController.updateAuthor)
 app.delete('/authors/:id', authorController.deleteAuthor)
+
+app.get('/novels', novelController.getAllNovels)
+app.get('/novels/:title', novelController.getOneNovel)
+app.post('/novels', novelController.createNovel)
+app.put('/novels/:title', novelController.updateNovel)
+app.delete('/novels/:title', novelController.deleteNovel)
+
+app.get('/specials', specialController.getAllSpecials)
+app.get('/specials/:title', specialController.getOneSpecial)
+app.post('/specials', specialController.createSpecial)
+app.put('/specials/:title', specialController.updateSpecial)
+app.delete('/specials/:title', specialController.deleteSpecial)
 
 app.listen(PORT, () => console.log(`Listening on port:${PORT}`))
